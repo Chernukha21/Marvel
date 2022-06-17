fba540505d7ac8dec736a11cc33a2424ec367932
Sasha <Chorniy2008@ukr.net>
2022-06-17 19:48:14 +0300

    commit before pushing

---
+++ src/components/error/Error.js
@@ -0,0 +1,18 @@
+import errorGif from './error.gif';
+import React from 'react';
+
+const ErrorMessage = () => {
+    return(
+        <img
+            style={{
+                display: 'block',
+                width: '250px',
+                height: '250px',
+                objectFit: 'contain',
+                margin: '0 auto'
+            }}
+            src={errorGif} alt="error"
+        />
+    )
+}
+export default ErrorMessage;
