7be6f76adaa1ce9541db074d9c75cc79c02e46ce
Sasha <Chorniy2008@ukr.net>
2022-07-08 14:30:59 +0300

    responsive layout add changes

--- src/components/appHeader/AppHeader.js
+++ src/components/appHeader/AppHeader.js
@@ -1,52 +1,50 @@
 import React from 'react';
-import {Link,NavLink} from "react-router-dom";
+import {Link, NavLink} from "react-router-dom";
 import './appHeader.scss';
 
 const AppHeader = () => {
     return (
-        <div className="container">
-            <header className="app__header">
-                <h1 className="app__title">
-                    <Link to="/">
-                        <span>Marvel</span> information portal
-                    </Link>
-                </h1>
-                <nav className="app__menu">
-                    <ul>
-                        <li>
-                            <NavLink
-                                exact
-                                activeStyle={{
-                                    'color':'red',
-                                    'padding': '0 5px 0 5px',
-                                    'display':'block',
-                                    'borderRadius': '5px',
-                                    'border': '1px solid black'
-                                }}
-                                to="/"
-                            >
-                                Characters
-                            </NavLink>
-                        </li>
-                        /
-                        <li>
-                            <NavLink
-                                activeStyle={{
-                                    'color':'red',
-                                    'padding': '0 5px 0 5px',
-                                    'display':'block',
-                                    'borderRadius': '5px',
-                                    'border': '1px solid black'
-                                }}
-                                to="/comics"
-                            >
-                                Comics
-                            </NavLink>
-                        </li>
-                    </ul>
-                </nav>
-            </header>
-        </div>
+        <header className="app__header">
+            <h1 className="app__title">
+                <Link to="/">
+                    <span>Marvel</span> information portal
+                </Link>
+            </h1>
+            <nav className="app__menu">
+                <ul>
+                    <li>
+                        <NavLink
+                            exact
+                            activeStyle={{
+                                'color': 'red',
+                                'padding': '0 5px 0 5px',
+                                'display': 'block',
+                                'borderRadius': '5px',
+                                'border': '1px solid black'
+                            }}
+                            to="/"
+                        >
+                            Characters
+                        </NavLink>
+                    </li>
+                    /
+                    <li>
+                        <NavLink
+                            activeStyle={{
+                                'color': 'red',
+                                'padding': '0 5px 0 5px',
+                                'display': 'block',
+                                'borderRadius': '5px',
+                                'border': '1px solid black'
+                            }}
+                            to="/comics"
+                        >
+                            Comics
+                        </NavLink>
+                    </li>
+                </ul>
+            </nav>
+        </header>
     )
 }
 
