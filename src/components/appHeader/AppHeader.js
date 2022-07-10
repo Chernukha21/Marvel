6bd5b7ba4875d6a37e806603c5f6d138ee1b8fda
Sasha <Chorniy2008@ukr.net>
2022-07-10 19:17:49 +0300

    Added wrapper style

--- src/components/appHeader/AppHeader.js
+++ src/components/appHeader/AppHeader.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import {Link, NavLink} from "react-router-dom";
 import './appHeader.scss';
+import {Wrapper} from "./AppHeader.style";
 
 const AppHeader = () => {
     return (
-        <header className="app__header">
+        <Wrapper>
             <h1 className="app__title">
                 <Link to="/">
-                    <span>Marvel</span> information portal
+                    Marvel information portal
                 </Link>
             </h1>
             <nav className="app__menu">
@@ -44,7 +45,7 @@ const AppHeader = () => {
                     </li>
                 </ul>
             </nav>
-        </header>
+        </Wrapper>
     )
 }
 
