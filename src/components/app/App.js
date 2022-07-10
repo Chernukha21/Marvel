b3bba4d7c3fdbe2f112fa5a134068faa11478e53
Sasha <Chorniy2008@ukr.net>
2022-07-10 18:57:51 +0300

    mooved pages from components directory

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -3,10 +3,10 @@ import {BrowserRouter as Router, Route,Switch} from 'react-router-dom';
 import AppHeader from "../appHeader/AppHeader";
 import Spinner from "../spinner/Spinner";
 
-const Page404 = lazy(() => import('../pages/404'))
-const MainPage = lazy(() => import('../pages/MainPage'));
-const ComicsPage = lazy(() => import('../pages/ComicsPage'));
-const SingleComicPage = lazy(() => import('../pages/SingleComicPage'));
+const Page404 = lazy(() => import('../../pages/404'))
+const MainPage = lazy(() => import('../../pages/MainPage'));
+const ComicsPage = lazy(() => import('../../pages/ComicsPage'));
+const SingleComicPage = lazy(() => import('../../pages/SingleComicPage'));
 
 
 const App = () => {
