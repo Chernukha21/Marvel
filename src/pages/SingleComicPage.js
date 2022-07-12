2c895e5894a807e5e1b6b22b4b72c6556b82c9b4
Sasha <Chorniy2008@ukr.net>
2022-07-12 19:21:42 +0300

    added styled componets style for single-comic-page

--- src/pages/SingleComicPage.js
+++ src/pages/SingleComicPage.js
@@ -1,10 +1,11 @@
 import { useParams, Link } from 'react-router-dom';
 import React,{ useState, useEffect } from 'react';
-import styled from 'styled-components';
 import useMarvelService from '../services/MarvelService';
 import Spinner from '../components/spinner/Spinner';
 import ErrorMessage from '../components/error/Error';
 import AppBanner from "../components/appBanner/AppBanner";
+import {Container} from "../components/app/App.style";
+import {Wrapper, Picture, InfoWrapper, Title, Description, Price, StyledLink} from "./SingleComicPage.style";
 import './singleComicPage.scss';
 
 const SingleComicPage = () => {
@@ -44,19 +45,19 @@ const View = ({comic}) => {
     const {title, description, pageCount, thumbnail, language, price} = comic;
 
     return (
-        <div className="container">
-            <div className="single-comic">
-                <img src={thumbnail} alt={title} className="single-comic__img"/>
-                <div className="single-comic__info">
-                    <h2 className="single-comic__name">{title}</h2>
-                    <p className="single-comic__descr">{description}</p>
-                    <p className="single-comic__descr">{pageCount}</p>
-                    <p className="single-comic__descr">Language: {language}</p>
-                    <div className="single-comic__price">{price}</div>
-                </div>
-                <Link to="/comics" className="single-comic__back">Back to all</Link>
-            </div>
-        </div>
+        <Container>
+            <Wrapper>
+                <Picture src={thumbnail} alt={title}/>
+                <InfoWrapper>
+                    <Title className="single-comic__name">{title}</Title>
+                    <Description>{description}</Description>
+                    <Description>{pageCount}</Description>
+                    <Description>Language: {language}</Description>
+                    <Price>{price}</Price>
+                </InfoWrapper>
+                <StyledLink to="/comics" className="single-comic__back">Back to all</StyledLink>
+            </Wrapper>
+        </Container>
     )
 }
 
