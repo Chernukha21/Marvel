f5d5d2758da435c721c6b4369dc15455535e1f83
Sasha <Chorniy2008@ukr.net>
2022-07-25 16:03:25 +0300

    Adaptive layout of components charlist, charinfo was finished

--- src/components/charList/CharList.style.js
+++ src/components/charList/CharList.style.js
@@ -4,6 +4,7 @@ import THEME from "../../theme";
 
 export const CharactersList = styled.div`
     text-align: center;
+    order: 2;
     span {
         display: block;
         font-size: 30px;
@@ -16,21 +17,51 @@ export const Wrapper = styled.ul`
     grid-template-columns: repeat(3, 200px);
     column-gap: 25px;
     row-gap: 30px;
+    @media ${THEME.MEDIA.largeTablet}{
+        grid-template-columns: repeat(2, 1fr);
+    }
+    @media ${THEME.MEDIA.miniTablet}{
+        grid-template-columns: minmax(auto,1fr);
+        column-gap: 5px;
+        row-gap: 5px;
+    }
+    @media ${THEME.MEDIA.phone}{
+        grid-template-columns: minmax(auto,1fr);
+        column-gap: 5px;
+        row-gap: 5px;
+    }
+    
 `
 export const Item = styled.li`
-    width: 200px;
+    min-width: 200px;
     height: 318px;
     background-color: ${THEME.COLOR.SECONDARY};
     box-shadow: 5px 5px 10px rgba(0, 0, 0, .25);
     padding: 15px;
     cursor: pointer;
     transition: 0.3s transform;
+    @media ${THEME.MEDIA.largeTablet}{
+        padding: 0;
+    }
+     @media ${THEME.MEDIA.miniTablet}{
+        padding: 0;
+    }
 `
 export const Image = styled.img`
-    width: 200px;
+    width: 100%;
     height: 200px;
     object-fit: cover;
     transform: translate(-15px, -15px);
+    @media ${THEME.MEDIA.largeTablet}{
+        transform: none;
+    }
+    @media ${THEME.MEDIA.miniTablet}{
+        transform: none;
+    }
+    @media ${THEME.MEDIA.phone}{
+        transform: none;
+    }
+    
 `
 export const Name = styled.div`
     font-weight: bold;
@@ -38,5 +69,8 @@ export const Name = styled.div`
     line-height: 29px;
     text-transform: uppercase;
     color: #fff;
+    @media ${THEME.MEDIA.largeTablet}{
+        padding: 20px 0 0 0;
+    }
 `
 
